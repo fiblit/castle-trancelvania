612de7c9a7ba786703ceb3f49fefddfc4c44a952
Dalton Hildreth <djdjhildreth@gmail.com>
2017-07-28 03:16:43 -0500

    Add snappy "momentous" motion for player
    The motion has a certain 'weight' to its feeling. (No realistic physics is
    happening, though. Please bear that in mind.) The acceleration is quick,
    though, which is good for a bullet hell game with perhaps a measure of
    dodging involved. The weight to your motion, though, provides what feels like a
    bit of a backpedal almost. Letting yourself stop is gentle and slow, which
    feels, at least somewhat, realistic.

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,18 +1,47 @@
 extends KinematicBody2D
 
-export var max_speed = 100
-var velocity = Vector2(0,0)
+export var max_speed = 200
+export var time_to_deccel = 0.6
+export var time_to_accel = 0.15
+var deccel = Vector2(0, 0)
+var accel = Vector2(0, 0)
+var velocity = Vector2(0, 0)
+var pos = Vector2(0, 0)
 
-func move(vec):
-	velocity = (velocity + vec)
+func deccel():
+	deccel = (1 / time_to_deccel) * velocity
+	accel = Vector2(0, 0)
+
+func accel(vec):
+	deccel = Vector2(0, 0) #prevents the jerking turn-around (maybe wanted)
+	accel = vec.normalized() * max_speed / time_to_accel
+
+func _fixed_process(delta):
+	#This is poor integration, however it doesn't need to be good. The character
+	#already has unrealistic physics. If it drifts from reality over time, that
+	#is fine. Acceleration is instantaneous, but decceleration is not.
+	
+	velocity += accel * delta
+	
+	#if deccelerating
+	if deccel.length_squared() > 0:
+		velocity -= deccel * delta
+		
+		#If velocity has been deccelerated to the point that it moves back a bit
+		if velocity.dot(-deccel) > 0:
+			#stop moving!
+			velocity = Vector2(0, 0)
+			deccel = Vector2(0, 0)
+	
 	var speed = velocity.length_squared()
 	if speed > max_speed * max_speed:
 		velocity = velocity.normalized() * max_speed
-
-func _fixed_process(delta):
-	set_pos(velocity * delta + get_pos())
+	
+	pos += velocity * delta
+	set_pos(pos)
 
 func _ready():
+	pos = get_pos()
 	set_process(true)
 	set_fixed_process(true)
 	
--- game/scenes/main.gd
+++ game/scenes/main.gd
@@ -3,20 +3,42 @@ extends Node2D
 var bullet_scene = preload("res://game/bullet.tscn")
 var player = null
 
+var is_action_held = {
+	"char_attack" : false,
+	"char_move_up" : false,
+	"char_move_down" : false,
+	"char_move_left" : false,
+	"char_move_right" : false}
+
+func try_hold_actions(event):
+	for a in is_action_held.keys():
+		if event.is_action_pressed(a):
+			is_action_held[a] = true
+		elif event.is_action_released(a):
+			is_action_held[a] = false
+
 func _input(event):
+	try_hold_actions(event)
+	
 	if event.is_action_pressed("char_attack"):
 		var dir = get_viewport().get_mouse_pos()
 		var pos = player.get_pos()
 		dir = dir - pos
 		spawn_bullet(dir, pos)
-	elif event.is_action_pressed("char_move_up"):
-		player.move(Vector2(0, -10))
-	elif event.is_action_pressed("char_move_down"):
-		player.move(Vector2(0, 10))
-	elif event.is_action_pressed("char_move_left"):
-		player.move(Vector2(-10, 0))
-	elif event.is_action_pressed("char_move_right"):
-		player.move(Vector2(10, 0))
+
+	var motion = Vector2(0, 0)
+	if is_action_held["char_move_up"]:
+		motion += Vector2(0, -1)
+	if is_action_held["char_move_down"]:
+		motion += Vector2(0, 1)
+	if is_action_held["char_move_left"]:
+		motion += Vector2(-1, 0)
+	if is_action_held["char_move_right"]:
+		motion += Vector2(1, 0)
+	if motion.length_squared() > 0:
+		player.accel(motion)
+	else:
+		player.deccel()
 
 func spawn_bullet(dir, pos):
 	var node = bullet_scene.instance()
